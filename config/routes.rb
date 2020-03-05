5c5e9f136eb6958c64cd89236d22f3c3c5169815
Uchoosecode <awittyl44@gmail.com>
2020-03-05 13:22:45 -0500

    new project files

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :toys
+  resources :children
+  resources :toy_boxes
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
